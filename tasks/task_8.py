e106d4553d8bf732e70147753372bfa36a3a3ac6
Alvin <darkwell463@gmail.com>
2023-07-24 15:21:59 +0300

    Решение задач:  8

--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,1 +1,21 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+num_1 = float(input('Введите первое число: '))
+num_2 = float(input('Введите второе число: '))
+operation = input('Введите магическую операцию: ')
+operations = ["Призыв",
+              "Трансформация",
+              "Объединение",
+              "Исчезновение"]
+
+if operation == operations[0]:
+    print("Сумма магических сил чисел:", num_1 + num_2)
+elif operation == operations[1]:
+    print(abs(num_1) + abs(num_2))
+elif operation == operations[2]:
+    print('Объединение чисел:', num_1 * num_2)
+elif operation == operations[3]:
+    if num_2 == 0:
+        print('Ошибка: Второе число равно нулю!')
+    if num_2 != 0:
+        print('Исчезновение числа:', num_1 * 0)
+else:
+    print('Ошибка: Некорректная операция')
