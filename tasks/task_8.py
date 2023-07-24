c32e4bc438cc54520424d0e1bd7e1aaa21cd0437
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-07-24 16:34:39 +0100

    Проверка ДЗ 6.Условные операторы
    - Правильно: 7, 9
    - Необходимо улучшить: 7, 9
    - На доработку: 8

--- tasks/task_8.py
+++ tasks/task_8.py
@@ -9,6 +9,7 @@ operations = ["Призыв",
 if operation == operations[0]:
     print("Сумма магических сил чисел:", num_1 + num_2)
 elif operation == operations[1]:
+    # Работает некорректно, также отсутствует текст вывода
     print(abs(num_1) + abs(num_2))
 elif operation == operations[2]:
     print('Объединение чисел:', num_1 * num_2)
@@ -16,6 +17,9 @@ elif operation == operations[3]:
     if num_2 == 0:
         print('Ошибка: Второе число равно нулю!')
     if num_2 != 0:
+        # Неправильно работает операция "Исчезновение"
         print('Исчезновение числа:', num_1 * 0)
 else:
     print('Ошибка: Некорректная операция')
+
+# В целом все хорошо, исправьте ошибки. Также добавьте пожалуйста аннотации.
