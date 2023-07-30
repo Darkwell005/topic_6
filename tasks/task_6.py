ae311484d3fa2089b61bacbe0a3bf1eb67405cbf
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-07-30 12:56:52 +0100

    ДЗ 6.Условные операторы
    - Правильно: 5
    - Необходимо улучшить: 6
    - На доработку: 6, 10

--- tasks/task_6.py
+++ tasks/task_6.py
@@ -2,12 +2,15 @@ year = int(input("Введите год: "))
 month = int(input("Введите номер месяца: "))
 
 leap_month = [1, 3, 5, 7, 8, 10, 12]
-
-
 is_leap: bool = year % 4 == 0 or year % 100 != 0 or year % 400 == 0
+
 if is_leap:
     if month not in leap_month:
         print("Нет")
     if month in leap_month:
         print("Да")
 
+# Если год является високосным и в месяце 31 день
+#    выводим "Да"
+# Иначе
+#    "Нет"
