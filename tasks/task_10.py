b36757769ea2c3560e8e349831352f9027d7f56c
Alvin <darkwell463@gmail.com>
2023-07-30 14:03:42 +0300

    Решение задач:  6,10

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -6,8 +6,27 @@ ALPHABETS = {
 }
 
 print("Добро пожаловать в программу \"Буква-Детектив\"!", end="\n\n")
-type_alphabets = ["Cyrillic", "Latin"]
-user_input = int(input('Выберите алфавит:'))
+
+print("Выберите алфавит:\n1. Латинский\n2. Кириллица")
+
+type_alphabets = ["Латинский", "Кириллица"]
+num_alphabets = int(input("Введите номер алфавита: "))
+
+
+if num_alphabets == 1:
+    let = input("Введите букву латинского алфавита: ")
+if let in ALPHABETS["en_vowels"]:
+    print(let, "- Гласная буква")
+if let in ALPHABETS["en_consonants"]:
+    print(let, "- Согласная буква")
+
+elif num_alphabets == 2:
+    let_2 = input("Введите букву кириллицы: ")
+
+if let in ALPHABETS["ru_vowels"]:
+    print(let, "- Гласная буква")
+if let in ALPHABETS["ru_consonants"]:
+    print(let, "- Согласная буква")
 
 
 
--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,5 +1,13 @@
-year = int(input('Введите год: '))
-month = int(input('Введите номер месяца: '))
+year = int(input("Введите год: "))
+month = int(input("Введите номер месяца: "))
+
+leap_month = [1, 3, 5, 7, 8, 10, 12]
 
 
+is_leap: bool = year % 4 == 0 or year % 100 != 0 or year % 400 == 0
+if is_leap:
+    if month not in leap_month:
+        print("Нет")
+    if month in leap_month:
+        print("Да")
 
