f995d52a3f1ea98df9ed2a892164f7c11e79ac1b
Alvin <darkwell463@gmail.com>
2023-07-30 16:53:49 +0300

    Решение задач:  10

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -1,4 +1,4 @@
-ALPHABETS = {
+ALPHABETS:dict = {
     "en_vowels": "AEIOU",
     "en_consonants": "BCDFGHJKLMNPQRSTVWXYZ",
     "ru_vowels": "АЕЁИОУЫЭЮЯ",
@@ -9,30 +9,26 @@ print("Добро пожаловать в программу \"Буква-Дет
 
 print("Выберите алфавит:\n1. Латинский\n2. Кириллица")
 
+alphabets: list = [1, 2]
+num_alphabets: int = int(input("Введите номер алфавита: "))
 
-num_alphabets = int(input("Введите номер алфавита: "))
-if num_alphabets != 1 or 2:
+if num_alphabets not in alphabets:
     print("Упс! Выбран неверный режим. Попробуйте ещё раз...")
-# I. Обработать самую худшую ситуация первым пункт 1.3
-# II. Если все нормально, прошли проверку, то дальше будем работать
-#   II.I. Реализуется пункт 1.1, 1.2
-#   II.II Реализуется Пункты, 3, 4, 5
-
-elif num_alphabets == 1:
+if num_alphabets == 1:
     let = input("Введите букву латинского алфавита: ")
-    if let not in ALPHABETS["en_consonants", "en_vowels"]:
-        print("Упс! Неизвестная буква. Попробуйте другую!")
-    elif let in ALPHABETS["en_vowels"]:
+    if let in ALPHABETS["en_vowels"]:
         print(let, "- Гласная буква")
     elif let in ALPHABETS["en_consonants"]:
         print(let, "- Согласная буква")
+    else:
+        print("Упс! Неизвестная буква. Попробуйте другую!")
+
 else:
     if num_alphabets == 2:
-        let_2 = input("Введите букву кириллицы: ")
-        if let_2 not in ALPHABETS["ru_vowels", "ru_consonants"]:
+        let = input("Введите букву кириллицы: ")
+        if let in ALPHABETS["ru_consonants"]:
+            print(let, "- Согласная буква")
+        elif let in ALPHABETS["ru_vowels"]:
+            print(let, "- Гласная буква")
+        else:
             print("Упс! Неизвестная буква. Попробуйте другую!")
-
-        elif let_2 in ALPHABETS["ru_vowels"]:
-            print(let_2, "- Гласная буква")
-        elif let_2 in ALPHABETS["ru_consonants"]:
-            print(let_2, "- Согласная буква")
