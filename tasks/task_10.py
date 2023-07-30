30faef030d4a81f2815d5400132b1618a22cd9fc
Alvin <darkwell463@gmail.com>
2023-07-30 15:15:51 +0300

    Решение задач:  10

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -9,27 +9,30 @@ print("Добро пожаловать в программу \"Буква-Дет
 
 print("Выберите алфавит:\n1. Латинский\n2. Кириллица")
 
-type_alphabets = ["Латинский", "Кириллица"]
-num_alphabets = int(input("Введите номер алфавита: "))
 
+num_alphabets = int(input("Введите номер алфавита: "))
+if num_alphabets != 1 or 2:
+    print("Упс! Выбран неверный режим. Попробуйте ещё раз...")
 # I. Обработать самую худшую ситуация первым пункт 1.3
 # II. Если все нормально, прошли проверку, то дальше будем работать
 #   II.I. Реализуется пункт 1.1, 1.2
 #   II.II Реализуется Пункты, 3, 4, 5
 
-if num_alphabets == 1:
+elif num_alphabets == 1:
     let = input("Введите букву латинского алфавита: ")
-    if let in ALPHABETS["en_vowels"]:
+    if let not in ALPHABETS["en_consonants", "en_vowels"]:
+        print("Упс! Неизвестная буква. Попробуйте другую!")
+    elif let in ALPHABETS["en_vowels"]:
         print(let, "- Гласная буква")
     elif let in ALPHABETS["en_consonants"]:
         print(let, "- Согласная буква")
-    else:
-        pass
+else:
+    if num_alphabets == 2:
+        let_2 = input("Введите букву кириллицы: ")
+        if let_2 not in ALPHABETS["ru_vowels", "ru_consonants"]:
+            print("Упс! Неизвестная буква. Попробуйте другую!")
 
-elif num_alphabets == 2:
-    let_2 = input("Введите букву кириллицы: ")
-
-if let in ALPHABETS["ru_vowels"]:
-    print(let, "- Гласная буква")
-if let in ALPHABETS["ru_consonants"]:
-    print(let, "- Согласная буква")
+        elif let_2 in ALPHABETS["ru_vowels"]:
+            print(let_2, "- Гласная буква")
+        elif let_2 in ALPHABETS["ru_consonants"]:
+            print(let_2, "- Согласная буква")
