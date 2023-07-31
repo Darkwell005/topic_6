f3f783d8b525a8739c485d06678c513904443943
Alvin <darkwell463@gmail.com>
2023-07-31 12:23:42 +0300

    Решение задач:  6

--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,16 +1,11 @@
-year = int(input("Введите год: "))
-month = int(input("Введите номер месяца: "))
+year: int = int(input("Введите год: "))
+month: int = int(input("Введите номер месяца: "))
 
-leap_month = [1, 3, 5, 7, 8, 10, 12]
-is_leap: bool = year % 4 == 0 or year % 100 != 0 or year % 400 == 0
+leap_month: list = [1, 3, 5, 7, 8, 10, 12]
+is_leap: bool = year % 4 == 0 and year % 100 != 0 or year % 400 == 0
 
-if is_leap:
-    if month not in leap_month:
-        print("Нет")
-    if month in leap_month:
-        print("Да")
+if is_leap and month in leap_month:
+    print("Да")
 
-# Если год является високосным и в месяце 31 день
-#    выводим "Да"
-# Иначе
-#    "Нет"
+else:
+    print("Нет")
