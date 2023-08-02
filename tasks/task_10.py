9921ba3e8ce35bd55f5cb932516fdbc33190614b
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-08-02 12:41:34 +0100

    Проверка ДЗ 6.Условные операторы
    - Разбор задач: №6, №10

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -6,35 +6,53 @@ ALPHABETS: dict = {
 }
 
 print("Добро пожаловать в программу \"Буква-Детектив\"!", end="\n\n")
-
-print("Выберите алфавит:\n1. Латинский\n2. Кириллица")
+print("Выберите алфавит:\n1. Латинский\n2. Кириллица\n")
 
 alphabets: list = [1, 2]
 num_alphabets: int = int(input("Введите номер алфавита: "))
 
-if num_alphabets not in alphabets:  # Отлично!
+if num_alphabets not in alphabets:
     print("Упс! Выбран неверный режим. Попробуйте ещё раз...")
-if num_alphabets == 1:  # А вот тут беда. Что если, выбран неверный алфавит,
-    # верхний if выведет сообщение об ошибке, а этот блок условий в любом
-    # случае будет проверяется, разве это логично?
-    let = input("Введите букву латинского алфавита: ")
-    if let in ALPHABETS["en_vowels"]:
-        print(let, "- Гласная буква")
-    elif let in ALPHABETS["en_consonants"]:
-        print(let, "- Согласная буква")
-    else:
-        print("Упс! Неизвестная буква. Попробуйте другую!")
-
 else:
-    if num_alphabets == 2:  # Это лишняя проверка, если num_alphabets == 1
-        # ложно то в любом случае это 2, опять это будет правильно,
-        # после того, как исправите замечание выше
+    if num_alphabets == 1:
+        let = input("Введите букву латинского алфавита: ")
+        if let in ALPHABETS["en_vowels"]:
+            print(let, "- Гласная буква")
+        elif let in ALPHABETS["en_consonants"]:
+            print(let, "- Согласная буква")
+        else:
+            print("Упс! Неизвестная буква. Попробуйте другую!")
+    else:
         let = input("Введите букву кириллицы: ")
         if let in ALPHABETS["ru_consonants"]:
             print(let, "- Согласная буква")
         elif let in ALPHABETS["ru_vowels"]:
             print(let, "- Гласная буква")
-        else:  # одно и тоже повторяется, смотрите на строку 26
+        else:
             print("Упс! Неизвестная буква. Попробуйте другую!")
 
-#  В целом у Вас хорошая реализация, необходимо немного улучшить
+# --------------------
+
+# DRY - Don't Repeat Yourself
+alphabets: list = [1, 2]
+num_alphabets: int = int(input("Введите номер алфавита: "))
+
+if num_alphabets not in alphabets:
+    print("Упс! Выбран неверный режим. Попробуйте ещё раз...")
+else:
+    vowels = consonants = input_hint = None
+    if num_alphabets == 1:
+        vowels = ALPHABETS["en_vowels"]
+        consonants = ALPHABETS["en_consonants"]
+        input_hint = "Введите букву латинского алфавита: "
+    else:
+        vowels = ALPHABETS["ru_vowels"]
+        consonants = ALPHABETS["ru_consonants"]
+        input_hint = "Введите букву кириллицы: "
+
+    if (let := input(input_hint)) in vowels:
+        print(let, "- Гласная буква")
+    elif let in consonants:
+        print(let, "- Согласная буква")
+    else:
+        print("Упс! Неизвестная буква. Попробуйте другую!")
--- tasks/task_6.py
+++ tasks/task_6.py
@@ -2,11 +2,8 @@ year: int = int(input("Введите год: "))
 month: int = int(input("Введите номер месяца: "))
 
 leap_month: list = [1, 3, 5, 7, 8, 10, 12]
-is_leap: bool = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-
-# Отлично! Теперь операторы правильно расставлены,
-# но есть нюанс, если не обернуть в скобки соответствующую
-# пару, программа может выдать неверный результат
+# is_leap: bool = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+is_leap: bool = (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
 
 if is_leap and month in leap_month:
     print("Да")
